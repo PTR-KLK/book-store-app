7b55f33762715f4a6e051627d8147c8685d786b0
Piotr Kielak <p.kielak@outlook.com>
2020-11-24 20:48:26 +0100

    cart stylesheets

--- front/src/features/Cart/Cart.js
+++ front/src/features/Cart/Cart.js
@@ -8,17 +8,33 @@ const Cart = () => {
   const cart = useSelector(selectCart);
 
   return (
-    <section>
-      <ul>
-        {cart.map((el) => {
-          return (
-            <li key={el.id}>
-              <CartItem data={el} />
-            </li>
-          );
-        })}
+    <section className="cartSection">
+      <ul className="cartSection__bookList">
+        {cart.length > 0 &&
+          cart.map((el, idx) => {
+            return (
+              <li key={el.id}>
+                <CartItem data={el} idx={idx + 1} />
+              </li>
+            );
+          })}
       </ul>
-      {cart.length > 0 ? <Link to="/order">Order</Link> : <p>Order</p>}
+      <div className="cartSection__navigation">
+        {cart.length > 0 ? (
+          <Link className="cartSection__orderButton" to="/order">
+            Order
+          </Link>
+        ) : (
+          <>
+            <p className="cartSection__orderInfo">
+              There is nothing to order, yet.
+            </p>
+            <p className="cartSection__orderButton cartSection__orderButton--inactive">
+              Order
+            </p>
+          </>
+        )}
+      </div>
     </section>
   );
 };
--- front/src/features/Cart/components/CartItem.js
+++ front/src/features/Cart/components/CartItem.js
@@ -1,15 +1,18 @@
 import React from "react";
 import CartButton from "./CartButton";
 
-const Cart = ({ data }) => {
+const CartItem = ({ data, idx }) => {
   return (
-    <>
+    <article className="cartItem">
+      <h3>{idx}</h3>
       <img src={data.cover_url} alt={`${data.title} cover`} />
-      <p>{data.title}</p>
-      <p>Price: {`${data.price / 100} ${data.currency}`}</p>
-      <CartButton item={data} />
-    </>
+      <section>
+        <p>{data.title}</p>
+        <p>{`${(data.price / 100).toFixed(2)} ${data.currency}`}</p>
+        <CartButton item={data} />
+      </section>
+    </article>
   );
 };
 
-export default Cart;
+export default CartItem;
