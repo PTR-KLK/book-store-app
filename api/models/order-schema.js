65b40060fea7f810124a2e94317fbc70b71eb489
Piotr Kielak <p.kielak@outlook.com>
2020-11-25 11:50:47 +0100

    Update order-schema

--- api/models/order-schema.js
+++ api/models/order-schema.js
@@ -6,8 +6,8 @@ const orderSchema = Joi.object().options({ abortEarly: false }).keys({
     id: Joi.number().valid(books.map(book => book.id)).required(),
     quantity: Joi.number().min(1).required(),
   })).required(),
-  first_name: Joi.string().min(4).max(50).required(),
-  last_name: Joi.string().min(5).max(50).required(),
+  first_name: Joi.string().min(3).max(50).required(),
+  last_name: Joi.string().min(3).max(50).required(),
   city: Joi.string().required(),
   zip_code: Joi.string().regex(/\d{2}-\d{3}/).required(),
 });
